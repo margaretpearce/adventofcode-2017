8c7e70a4b01aa37ddfc854232663362839d2f724
Margaret Pearce <margaret.pearce@gmail.com>
2017-12-01 21:01:08 -0500

    Day 1 Puzzle 2

--- day1.py
+++ day1.py
@@ -1,5 +1,12 @@
 import sys
 digits = [int(x) for x in list(sys.argv[1])]
-output = sum([digits[x % len(digits)] if digits[x % len(digits)] == digits[(x + 1) % len(digits)] else 0
-          for x in range(0, len(digits))])
-print(output)
+
+# Puzzle 1
+puzzle1_output = sum([digits[x % len(digits)] if digits[x % len(digits)] == digits[(x + 1) % len(digits)] else 0
+                      for x in range(0, len(digits))])
+print(puzzle1_output)
+
+# Puzzle 2
+puzzle2_output = sum([digits[x % len(digits)] if digits[x % len(digits)] == digits[(x + (len(digits) // 2)) % len(digits)]
+                      else 0 for x in range(0, len(digits))])
+print(puzzle2_output)
