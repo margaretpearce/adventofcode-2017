68a38b890cc068bdb7622f9d7f7a7531deb299ed
Margaret Pearce <margaret.pearce@gmail.com>
2017-12-02 09:57:50 -0500

    pip changes

--- day1.py
+++ day1.py
@@ -2,11 +2,13 @@ import sys
 digits = [int(x) for x in list(sys.argv[1])]
 
 # Puzzle 1
-puzzle1_output = sum([digits[x % len(digits)] if digits[x % len(digits)] == digits[(x + 1) % len(digits)] else 0
+puzzle1_output = sum([digits[x % len(digits)]
+                      if digits[x % len(digits)] == digits[(x + 1) % len(digits)] else 0
                       for x in range(0, len(digits))])
 print(puzzle1_output)
 
 # Puzzle 2
-puzzle2_output = sum([digits[x % len(digits)] if digits[x % len(digits)] == digits[(x + (len(digits) // 2)) % len(digits)]
-                      else 0 for x in range(0, len(digits))])
+puzzle2_output = sum([digits[x % len(digits)]
+                      if digits[x % len(digits)] == digits[(x + (len(digits) // 2)) % len(digits)] else 0
+                      for x in range(0, len(digits))])
 print(puzzle2_output)
