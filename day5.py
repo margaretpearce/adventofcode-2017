5bac56da4f1328ea97e477a728fb7a31d2b59978
Margaret Pearce <margaret.pearce@gmail.com>
2017-12-07 23:10:56 -0500

    Day 5, Problem 2

--- day5.py
+++ day5.py
@@ -3,10 +3,25 @@ with open("input/day5_1.txt", "r") as input_data:
 
 instruction_number = 0
 total_steps = 0
+offsets_original = offsets.copy()
 
+# Part A
 while instruction_number < len(offsets):
     instruction_value = offsets[instruction_number]
     offsets[instruction_number] += 1
+    instruction_number += instruction_value
+    total_steps += 1
+
+print(total_steps)
+
+# Part B
+instruction_number = 0
+total_steps = 0
+offsets = offsets_original
+
+while instruction_number < len(offsets):
+    instruction_value = offsets[instruction_number]
+    offsets[instruction_number] += -1 if instruction_value >= 3 else 1
     instruction_number += instruction_value
     total_steps += 1
 
