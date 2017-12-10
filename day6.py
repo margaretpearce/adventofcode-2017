e3da52cac3165e44857865855329e5c3d5075432
Margaret Pearce <margaret.pearce@gmail.com>
2017-12-09 22:41:02 -0500

    Day 6, Problem 1

---
+++ day6.py
@@ -0,0 +1,23 @@
+import sys
+digits = [int(x) for x in sys.argv[1:]]
+
+redistribute_index = 0
+redistributed_value = 0
+cycles = 0
+seen_configurations = {}
+
+while str(digits) not in seen_configurations:
+    seen_configurations[str(digits)] = True
+    redistributed_value = max(digits)
+    redistribute_index = digits.index(redistributed_value)
+
+    digits[redistribute_index] = 0
+
+    while redistributed_value > 0:
+        redistribute_index = (redistribute_index + 1) % len(digits)
+        digits[redistribute_index] += 1
+        redistributed_value -= 1
+
+    cycles += 1
+
+print(cycles)
